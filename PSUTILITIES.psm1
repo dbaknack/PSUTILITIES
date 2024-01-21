a8e0b4dadd03c09310e7a19306b13565526971ae
DBAKnack <dbaknack@gmail.com>
2024-01-21 16:22:34 -0700

    cleaned up message output

--- PSUTILITIES.psm1
+++ PSUTILITIES.psm1
@@ -3,7 +3,6 @@ $PRIVATE_PATH = Join-Path $PSScriptRoot "Private"
 $private_files = Get-ChildItem -Path $PRIVATE_PATH -File -Filter "*.ps1" -Recurse
 foreach($file in $private_files){
     . $file.FullName
-    write-host  "$($file.FullName)" -ForegroundColor Blue
 }
 
 # load public functions/classes
@@ -11,5 +10,4 @@ $PRIVATE_PATH = Join-Path $PSScriptRoot "Public"
 $private_files = Get-ChildItem -Path $PRIVATE_PATH -File -Filter "*.ps1" -Recurse
 foreach($file in $private_files){
     . $file.FullName
-    write-host  "$($file.FullName)" -ForegroundColor Blue
 }
