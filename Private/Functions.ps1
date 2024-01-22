8ab2dbe7af1c046046c9b81859c2e93cabd13870
DBAKnack <dbaknack@gmail.com>
2024-01-21 20:49:00 -0700

    added functions

--- Private/Functions.ps1
+++ Private/Functions.ps1
@@ -1,6 +1,55 @@
-Function ModulePath{
-    $PSScriptRoot
+Function Platform {
+    $platform = @{
+        onMac = [System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::OSX)
+        onLinux = [System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Linux)
+        onWindows = [System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Windows)
+    }
+    foreach($os in $platform.keys){
+        $myOS = [string]
+        if($platform.$os){
+            $osFound = $true
+            $myOS =$os
+            break
+        }else{
+            $osFound = $false
+        }
+    }
+    $myOS
 }
 
-Function JsonConverter{[JsonConverter]::new()}
-Function PSUTILITIES {[PSUTILITIES]::new() }
+Function PlatformParameters{
+    $myPlatform = Platform
+    $osParameters = @{
+        Separator   = [string]
+        OS          = $myPlatform
+    }
+    switch($myPlatform){
+        "onMac"{
+            $osParameters.Separator = '/'
+        }
+        "onLinux"{
+            $osParameters.Separator = '/'
+        }
+        "onWindows"{
+            $osParameters.Separator = '\'
+        }
+    }
+    $osParameters
+}
+
+Function JsonConverter{
+    $myLocation         = Get-Location
+    $functionPath       = $myLocation.Path
+    $PlatformParameters = PlatformParameters
+    $Seperator          = $PlatformParameters.Separator
+    $classPath          = "{1}{0}{2}" -f $Seperator, $functionPath, "Classes.ps1"
+    
+    $JSONConverter = [JsonConverter]::new()
+    $JSONConverter
+}
+
+Function PSUTILITIES {
+    $PSUTILITIES =  [PSUTILITIES]::new()
+    $PSUTILITIES
+}
+
